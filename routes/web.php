ca120582c219ccb9a5b9d9072572e110aca1d3dd
eouannleveque <60744487+eouannleveque@users.noreply.github.com>
2026-08-18 22:31:24 +0200

    Delete routes directory

--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::middleware('auth')->group(function () {
-    Route::view('/me', 'profile.show')->name('profile.show');
-});
-
-require __DIR__.'/auth.php'; // genere par Breeze (login, register, password reset, verification email)
